9e5706c2017024c07787ca0727e1273dc57a542c
geetasathe1 <geeta.sathe@multigenesys.com>
2026-06-23 18:58:10 +0530

    Create NumberPattern.java

---
+++ NumberPattern.java
@@ -0,0 +1,13 @@
+public class NumberPattern {
+    public static void main(String[] args) {
+        int num = 1;
+
+        for (int row = 1; row <= 5; row++) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print(num + " ");
+                num++;
+            }
+            System.out.println();
+        }
+    }
+}
